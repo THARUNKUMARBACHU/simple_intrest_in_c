466d263eec9526ef72df715ef5a8772d1dd1fb09
THARUN KUMAR <btharunkumar218@gmail.com>
2022-01-03 20:33:22 +0530

    changed again

--- simple_intrest.cpp
+++ simple_intrest.cpp
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-	int principle_amount= 1000;  // INITIALISING AND DECLARING VALUE TO principle_amount
+	int principle_amount= 2000;  // INITIALISING AND DECLARING VALUE TO principle_amount
 	float rate_of_intrest = 0.5; // INITIALISING AND DECLARING VALUE TO rate_of_intrest
 	int time = 1;                //INITIALISING AND DECLARING VALUE  TO time 
 	float  simple_intrest= ((principle_amount*rate_of_intrest*time)/100); // FORMULA TO FIND SIMPLE INTREST 
